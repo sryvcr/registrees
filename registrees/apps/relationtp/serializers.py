81eb3279dd33b78d4cee72e02672e6e96b083e4d
sryvcr <andresgx9@gmail.com>
2018-09-06 12:30:47 -0500

    add user serializer

--- registrees/apps/relationtp/serializers.py
+++ registrees/apps/relationtp/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from registrees.apps.relationtp.models import Tree, Park
 
@@ -15,3 +16,11 @@ class TreeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tree
         fields = ('id', 'nombre', 'altura', 'edad_aproximada', 'parque')
+
+
+# serializer usuario
+class CurrentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User 
+        fields = ('first_name', 'last_name', 'email', 'is_staff', 
+                  'is_active', 'date_joined', 'last_login') 
